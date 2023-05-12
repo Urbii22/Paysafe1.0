71d62d4feada056307d819cac51e748dbc3639ff
motac <vdemarcovelascomarcos@gmail.com>
2023-05-12 04:07:45 +0200

    Empresa5

---
+++ app/src/main/java/com/example/example/MetodosCobro.java
@@ -0,0 +1,14 @@
+package com.example.example;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MetodosCobro extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_metodos_cobro);
+    }
+}
