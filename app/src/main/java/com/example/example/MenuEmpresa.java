4f1e1668344163ee430ee204d5a491a8751c3c8b
motac <vdemarcovelascomarcos@gmail.com>
2023-05-12 04:07:45 +0200

    Empresa5

--- app/src/main/java/com/example/example/MenuEmpresa.java
+++ app/src/main/java/com/example/example/MenuEmpresa.java
@@ -2,7 +2,12 @@ package com.example.example;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 public class MenuEmpresa extends AppCompatActivity {
 
@@ -10,5 +15,26 @@ public class MenuEmpresa extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu_empresa);
+        @SuppressLint({"MissingInflatedId", "LocalSuppress"}) ImageButton MetodoCobro = (ImageButton) findViewById(R.id.MetodoCobro);
+        @SuppressLint({"MissingInflatedId", "LocalSuppress"}) ImageButton CuentaEmpresa = (ImageButton) findViewById(R.id.CuentaEmpresa);
+        Button Recibir = (Button) findViewById(R.id.RecibirEmpresa);
+        MetodoCobro.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View view) {
+                startActivity(new Intent(MenuEmpresa.this, MetodosCobro.class));
+            }
+        });
+
+        CuentaEmpresa.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View view) {
+                startActivity(new Intent(MenuEmpresa.this, MetodosCobro.class));
+            }
+        });
+        Recibir.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View view) {
+                startActivity(new Intent(MenuEmpresa.this, MetodosCobro.class));
+            }
+        });
     }
+
+
 }
