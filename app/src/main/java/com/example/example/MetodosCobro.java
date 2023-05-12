0dda0cbf543a29b6f3d78da446dc2a2aba671d23
motac <vdemarcovelascomarcos@gmail.com>
2023-05-12 04:50:11 +0200

    Empresa6

--- app/src/main/java/com/example/example/MetodosCobro.java
+++ app/src/main/java/com/example/example/MetodosCobro.java
@@ -2,7 +2,11 @@ package com.example.example;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class MetodosCobro extends AppCompatActivity {
 
@@ -10,5 +14,12 @@ public class MetodosCobro extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_metodos_cobro);
+        @SuppressLint({"MissingInflatedId", "LocalSuppress"}) ImageButton Regresar = (ImageButton) findViewById(R.id.RegresarEmpresa);
+        Regresar.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View view) {
+                startActivity(new Intent(MetodosCobro.this, MenuEmpresa.class));
+            }
+        });
+
     }
 }
