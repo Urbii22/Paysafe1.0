b6cf8e85ebf4fe936bfcc55a3b605250ebf6b102
motac <vdemarcovelascomarcos@gmail.com>
2023-05-12 13:52:40 +0200

    VerificarUsuarioEmpresa

--- app/src/main/java/com/example/example/TarjetaEmpresa.java
+++ app/src/main/java/com/example/example/TarjetaEmpresa.java
@@ -2,7 +2,12 @@ package com.example.example;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 public class TarjetaEmpresa extends AppCompatActivity {
 
@@ -10,5 +15,17 @@ public class TarjetaEmpresa extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tarjeta_empresa);
+        Button Aceptar = (Button) findViewById(R.id.Aceptar);
+        @SuppressLint({"MissingInflatedId", "LocalSuppress"}) ImageButton Regresar = (ImageButton) findViewById(R.id.RegresarTarjeta);
+        Aceptar.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View view) {
+                startActivity(new Intent(TarjetaEmpresa.this, VerificarUsuarioEmpresa.class));
+            }
+        });
+        Regresar.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View view) {
+                startActivity(new Intent(TarjetaEmpresa.this, ElegirCobro.class));
+            }
+        });
     }
 }
